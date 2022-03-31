041f35d3508ad5814580ba96b0ed26824069e8c6
Vanessa <57110017+dev-Yogi@users.noreply.github.com>
2022-03-30 22:55:19 -0500

    config btsp/fnt-awesome/google-fnt

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,19 @@ import './App.css';
 function App() {
   return (
    <React.Fragment>
-     <div className="container">
+     <div className="container mt-3">
        <div className="gird">
          <div className="row">
            <div className="col">
-             <p className="h3">
+             <p className="h3 fw-bold text-success">
                App Component
              </p>
+             <p className='fs-italic'>
+               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis suscipit unde perferendis saepe facilis optio necessitatibus. Similique alias, fuga dignissimos molestiae itaque voluptatem vel ut dolor pariatur rerum nam minima?
+             </p>
+             <button className='btn btn-success btn-sm'>
+               <i className="fa fa-book"> </i>Read More
+             </button>
            </div>
          </div>
        </div>
