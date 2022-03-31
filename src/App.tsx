5d5a87ae374a5f0b2759e311245f13c6e807680e
Vanessa <57110017+dev-Yogi@users.noreply.github.com>
2022-03-30 22:32:54 -0500

    initial committ

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <React.Fragment>
+     <div className="container">
+       <div className="gird">
+         <div className="row">
+           <div className="col">
+             <p className="h3">
+               App Component
+             </p>
+           </div>
+         </div>
+       </div>
+     </div>
+   </React.Fragment>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/* Font Awesome */
+import "../node_modules/font-awesome/css/font-awesome.css";
+
+/* Bootstrap */
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
